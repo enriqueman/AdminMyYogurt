5da87b387686cf506001aef1c7ddeab56cd33c07
enriqueman <manzano.velasco217@gmail.com>
2022-12-08 04:17:29 -0500

    Conexión entre paginas
    conexion entre paginas con al lorden y main

--- app/src/main/java/edu/unicauca/aplimovil/adminmyyogurt/activity/AllOrderActivity.kt
+++ app/src/main/java/edu/unicauca/aplimovil/adminmyyogurt/activity/AllOrderActivity.kt
@@ -2,10 +2,10 @@ package edu.unicauca.aplimovil.adminmyyogurt.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import edu.unicauca.aplimovil.adminmyyogurt.R
 class AllOrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // setContentView(R.layout.activity_all_order)
+        setContentView(R.layout.activity_all_order)
     }
 }
--- app/src/main/java/edu/unicauca/aplimovil/adminmyyogurt/fragment/HomeFragment.kt
+++ app/src/main/java/edu/unicauca/aplimovil/adminmyyogurt/fragment/HomeFragment.kt
@@ -1,24 +1,39 @@
 package edu.unicauca.aplimovil.adminmyyogurt.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import edu.unicauca.aplimovil.adminmyyogurt.R
-//import edu.unicauca.aplimovil.adminmyyogurt.
+import edu.unicauca.aplimovil.adminmyyogurt.activity.AllOrderActivity
+import edu.unicauca.aplimovil.adminmyyogurt.databinding.FragmentHomeBinding
+
+//import edu.unicauca.aplimovil.adminmyyogurt.BuildConfig.
 
 class HomeFragment : Fragment() {
 
-private lateinit var binding : HomeFragment
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //binding= HomeFragment.inflate(layoutInflater)
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding= FragmentHomeBinding.inflate(layoutInflater)
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment3_to_categoryFragment)
+            }
+        binding.button2.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment3_to_yogurtFragment)
+        }
+        binding.button3.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment3_to_sliderFragment)
+        }
+        binding.button4.setOnClickListener {
+           startActivity(Intent(requireContext(),AllOrderActivity::class.java))
+        }
+        return binding.root
     }
 
 }
